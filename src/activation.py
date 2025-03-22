159af53e95c3b5529942640b6a7c55bf7d1236b1
Filbert88 <filbertfilbert21@gmail.com>
2025-03-22 21:29:11 +0700

    feat: add other activation fucntion

--- src/activation.py
+++ src/activation.py
@@ -28,3 +28,21 @@ def d_tanh(x):
 def softmax(x):
     exp_shifted = np.exp(x - np.max(x, axis=1, keepdims=True))
     return exp_shifted / np.sum(exp_shifted, axis=1, keepdims=True)
+
+def d_softmax(x):
+    s = softmax(x)
+    return s * (1 - s)  
+
+def leaky_relu(x, alpha=0.01):
+    return np.maximum(alpha * x, x)
+
+def d_leaky_relu(x, alpha=0.01):
+    dx = np.ones_like(x)
+    dx[x < 0] = alpha
+    return dx
+
+def elu(x, alpha=1.0):
+    return np.where(x > 0, x, alpha * (np.exp(x) - 1))
+
+def d_elu(x, alpha=1.0):
+    return np.where(x > 0, 1, alpha * np.exp(x))
