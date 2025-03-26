b6dbffd9313ca52f1c87aefbdc6882cada99f1f6
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2025-03-26 22:07:04 +0700

    feat: init weight

--- src/initialization.py
+++ src/initialization.py
@@ -12,3 +12,24 @@ def init_weights_normal(shape, mean, variance, seed=None):
     if seed is not None:
         np.random.seed(seed)
     return np.random.normal(mean, np.sqrt(variance), size=shape)
+
+def init_weights_xavier_uniform(shape, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    fan_in, fan_out = shape
+    limit = np.sqrt(6 / (fan_in + fan_out))
+    return np.random.uniform(-limit, limit, size=shape)
+
+def init_weights_xavier_normal(shape, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    fan_in, fan_out = shape
+    std = np.sqrt(2 / (fan_in + fan_out))
+    return np.random.normal(0, std, size=shape)
+
+def init_weights_he(shape, seed=None):
+    if seed is not None:
+        np.random.seed(seed)
+    fan_in, _ = shape
+    std = np.sqrt(2 / fan_in)
+    return np.random.normal(0, std, size=shape)
